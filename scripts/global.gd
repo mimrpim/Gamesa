66dbc654d8172af74464afa7c6bdb01a31fbfbf9
mimrpim <mimrpim@gmail.com>
2025-12-23 14:24:45 +0100

    added splash_scene and fixed bug

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,5 +1,7 @@
+extends Node
 
 const main_menu_scene = "res://scenes/main_menu.tscn"
+const splash_scene = "res://scenes/splash.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
