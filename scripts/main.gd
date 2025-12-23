e96f54245a96c9a35788f40d83a553755c211ed4
mimrpim <mimrpim@gmail.com>
2025-12-23 14:25:25 +0100

    fixe bug with error get_tree().change_scene_to_file(gv.splash_scene)

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,9 +2,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file(gv.splash_scene)
